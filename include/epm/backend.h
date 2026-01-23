7322c232e18f946bf300c7fe8d69c3b8ad5b57d6
Kenraali <aleksi.puhakainen@gmail.com>
2026-01-23 20:30:06 +0200

    Fix function signatures in backend.h

--- include/epm/backend.h
+++ include/epm/backend.h
@@ -19,13 +19,13 @@ typedef enum {
     EPM_ERR_EXECUTION
 } epm_status_t;
 
-epm_status_t epm_init(void);
-epm_distro_t epm_get_distro(void);
+epm_status_t epm_init(atom);
+epm_distro_t epm_get_distro(atom);
 
 
 epm_status_t epm_install_atom(const char *atom_file);
 epm_status_t epm_remove(const char *package_name);
-epm_status_t epm_list(void);
+epm_status_t epm_list(atom);
 
 const char *epm_strerror(epm_status_t status);
 const char *epm_distro_name(epm_distro_t distro);
@@ -34,3 +34,4 @@ const char *epm_distro_name(epm_distro_t distro);
 
 
 // There isn't much here yet. This is mainly vibe coded to have a semi functioning Backend & frontend connection. This may be the most vibe coded thing ever tho. "Latest commit: What the fuck did i just write" - Kenraali. 
+
